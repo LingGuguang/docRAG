49c4872be61702cbe5bb36650cf1eb1e053144eb
LingGuguang <1186986256@qq.com>
2024-05-12 12:41:15 +0800

    5/12

--- argparser.py
+++ argparser.py
@@ -5,10 +5,7 @@ def main_argparser():
     
     parser.add_argument('--hypocritical_answer', default=False, type=bool, help='generate a hypocritical answer to recall answer.')
     parser.add_argument('--additional_query', default=0, type=int, help='generate k additional query to recall answer.')
-    
 
-
-
-    return parser
+    return parser.parse_args()
 
 
