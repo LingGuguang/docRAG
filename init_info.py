fd49a816ea3e41c281e4d8275d3e898f31d904ba
LingGuguang <1186986256@qq.com>
2024-05-14 19:11:53 +0800

    5/14

--- init_info.py
+++ init_info.py
@@ -1,13 +1,12 @@
 
 from utils.basic_utils import read_json
-from utils.get_memory import SUI_MEMORY
 
 class InitInfo:
     intent_file_path = "./utils/intent.json"
     intent_set = read_json(intent_file_path).keys()
     
 
-class promptInfo:
+class PromptInfo:
     rag_text = None 
     intent = None 
     def __call__(self):
