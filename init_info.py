797b0b128bf340ab0a1260dde1159a98590353f7
LingGuguang <1186986256@qq.com>
2024-05-14 20:17:34 +0800

    5/14

--- init_info.py
+++ init_info.py
@@ -3,7 +3,7 @@ from utils.basic_utils import read_json
 
 class InitInfo:
     intent_file_path = "./utils/intent.json"
-    intent_set = read_json(intent_file_path).keys()
+    intent_set = [key for key in read_json(intent_file_path).keys()]
     
 
 class PromptInfo:
