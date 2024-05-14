71608e67e7b427a36a47907e7f7d59740264cfb9
LingGuguang <1186986256@qq.com>
2024-05-14 21:01:11 +0800

    5/14

--- utils/get_memory.py
+++ utils/get_memory.py
@@ -4,5 +4,6 @@ from langchain.memory import ConversationBufferMemory
 Sui_Memory = ConversationBufferMemory(
         human_prefix="观众",
         ai_prefix="岁己",
-        return_messages=True
+        return_messages=True,
+        memory_key='history'
         )
