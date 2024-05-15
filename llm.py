18bccbe7f6a9e6e5d3ada55f439b8a23aaaca159
LingGuguang <1186986256@qq.com>
2024-05-15 11:27:55 +0800

    5/14

--- llm.py
+++ llm.py
@@ -78,13 +78,14 @@ class QwenLLMChat(LLM):
                 **kwargs: Any):
 
         messages = [{"role": "user", "content": prompt}]
-        input_ids = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-        model_inputs = self.tokenizer([input_ids], return_tensors="pt").to('cuda')
-        generated_ids = self.model.generate(model_inputs.input_ids, max_new_tokens=512)
-        generated_ids = [
-            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-        ]
-        response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        # input_ids = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+        # model_inputs = self.tokenizer([input_ids], return_tensors="pt").to('cuda')
+        # generated_ids = self.model.generate(model_inputs.input_ids, max_new_tokens=512)
+        # generated_ids = [
+        #     output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+        # ]
+        # response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        response = self.model.chat(messages, self.tokenizer)
         
         return response
     
