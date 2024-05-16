05a5b83785baf29e6c55032a0edadbd677a7fd81
LingGuguang <1186986256@qq.com>
2024-05-16 19:16:24 +0800

    5/16

--- utils/intent_clear.py
+++ utils/intent_clear.py
@@ -2,11 +2,12 @@
 from typing import List
 import re
 
+
 SHORT_QUERY_AS_CHAT = 5
 
 def basic_query_intention_filter(init_query: str):
     if len(list(init_query)) < SHORT_QUERY_AS_CHAT:
-        
+        return True
 
 
 def intent_chain_after_filter(dirty_intent:str, intent_set: List[str]) -> int:
