22d7c8f375f44a1a6e6c088be4245ebab3ec5b5b
LingGuguang <1186986256@qq.com>
2024-05-19 21:33:30 +0800

    5/19

--- create_qa.py
+++ create_qa.py
@@ -1,6 +1,7 @@
 import chromadb 
 from llm import bceEmbeddingFunction, myChain, QwenLLMChat
 import os, sys
+from utils.basic_utils import save_json, save_text
 from utils.get_prompt import generate_query_from_dataset
 
 chromadb_name_for_save = "chaos_history"
@@ -10,25 +11,30 @@ print("get chromadb...")
 current_path = os.path.dirname(sys.path[0])
 model_name = "qwen1.5-14B-chat"
 
+save_qa = 'dataset/generate_qa.json'
+
 
 
 model_path = os.path.join(current_path, f"LLModel/{model_name}")
 chroma_client = chromadb.PersistentClient(chroma_path)
 bce_embedding_function = bceEmbeddingFunction(embedding_model_path)
 chroma_collection = chroma_client.get_or_create_collection(name=chromadb_name_for_save, embedding_function=bce_embedding_function)
-# llm = QwenLLMChat(model_path)
-# QAgeneraterChain = myChain(llm=llm,
-#                 prompt=generate_query_from_dataset(),
-#                 # memory=self.memory
-#                 )
+llm = QwenLLMChat(model_path)
+QAgeneraterChain = myChain(llm=llm,
+                prompt=generate_query_from_dataset(),
+                # memory=self.memory
+                )
 
 data = chroma_collection.get()
 data = [(ids, docs) for ids, docs in zip(data['ids'], data['documents'])] 
 print(len(data))
 print(data[0])
 
-
-# response = QAgeneraterChain.invoke()
+generate_qa_list = []
+for ids, docs in data[:10]:
+    temp = {}
+    response = QAgeneraterChain.invoke(docs)
+    generate_qa_list.append(response)
 
 
 
